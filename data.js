3eed1aece6bc47cc59854926c6983300af6fe1b9
Marco Abel Zefanya <marcoabel25@gmail.com>
2024-04-25 16:28:22 +0700

    Delete 1/2

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ let datas = [
     year: 2024,
     img: "https://m.media-amazon.com/images/M/MV5BODQ0NDhjYWItYTMxZi00NTk2LWIzNDEtOWZiYWYxZjc2MTgxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
     link: "https://www.youtube.com/watch?v=2QKg5SZ_35I",
-    actors: ["Tom Cruise", "Jenal"],
+    actors: "Tom Cruise, Jenal",
     description:
       "Jumanji adalah film thriller-fantasi Amerika Serikat yang dibuat pada tahun 1995. Disutradarai oleh Joe Johnston berdasarkan novel yang ditulis oleh Chris Van Allsburg pada tahun 1981. Film ini bercerita tentang sebuah permainan ajaib yang didalamnya terdapat binatang-binatang, dan binatang-binatang itu akan keluar jika tertera pada papan. Dan permainan akan usai jika salah satu dari pemain ada yang dapat mencapai titik Jumanji.",
   },
@@ -17,7 +17,7 @@ let datas = [
     year: 2000,
     img: "https://i.pinimg.com/736x/74/c1/18/74c118b4cffc66837af9a42e003da097.jpg",
     link: "https://www.youtube.com/watch?v=bNd5xfqVw1M",
-    actors: ["Shizuka", "Nobita"],
+    actors: "Shizuka, Nobita",
     description:
       "Doraemon) adalah judul sebuah manga dan anime populer yang dikarang Fujiko F. Fujio (藤子・F・不二雄, nama asli: Hiroshi Fujimoto) sejak Desember 1969 dan berkisah tentang kehidupan seorang anak pemalas kelas 5 sekolah dasar yang bernama Nobita Nobi (野比のび太) yang didatangi oleh sebuah robot kucing bernama Doraemon yang datang dari abad ke-22. Doraemon dikirim untuk menolong Nobita agar keturunannya dapat menikmati kesuksesannya daripada harus menderita dari utang finansial yang akan terjadi pada masa depan yang disebabkan karena kebodohan Nobita.",
   },
@@ -28,7 +28,7 @@ let datas = [
     year: 2003,
     img: "https://m.media-amazon.com/images/M/MV5BNTk2NzEyNTQtZTQ5MS00MjAyLTgzMDMtNDNkYTBkM2M2OTU3XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_FMjpg_UX1000_.jpg",
     link: "https://www.youtube.com/watch?v=a2cowVH03Xo",
-    actors: ["Spongebob", "Patrick", "Squidward"],
+    actors: "Spongebob, Patrick, Squidward",
     description:
       "SpongeBob SquarePants (atau lebih dikenal sebagai SpongeBob saja) adalah seri animasi terpopuler di jaringan Nickelodeon. Pada awalnya seri animasi ini ditayangkan pada 1999 di Amerika Serikat dan diciptakan oleh almarhum Stephen Hillenburg, seorang animator dan ahli biologi laut, dan diterbitkan oleh perusahaannya, United Plankton Pictures Inc. Seri animasi ini berlatar di Samudra Pasifik, yaitu di kota fiksi Bikini Bottom.",
   },
@@ -39,7 +39,7 @@ let datas = [
     year: 2000,
     img: "https://upload.wikimedia.org/wikipedia/id/5/55/WDKI_reborn.jpg",
     link: "https://www.youtube.com/watch?v=T0ki7KwvO1Q",
-    actors: ["Dono", "Kasino", "Indro"],
+    actors: "Dono, Kasino, Indro",
     description:
       "Warung Kopi, lebih dikenal dengan Warkop, sebelumnya bernama Warkop Prambors (1973-1985) dan Warkop DKI (1986-1997), merupakan grup lawak asal Indonesia yang dibentuk oleh Nanu Moeljono, Rudy Badil, Kasino Hadiwibowo, Wahjoe Sardono dan Indrodjojo Kusumonegoro, mereka pertama kali bertemu dalam program radio Obrolan Santai di Warung Kopi garapan Temmy Lesanpura, Kepala Bagian Programming Radio Prambors Rasisonia.",
   },
@@ -50,7 +50,7 @@ let datas = [
     year: 2007,
     img: "https://esensi.tv/wp-content/uploads/2023/09/2fast2furious.jpg",
     link: "https://www.youtube.com/watch?v=ZZGkV_xWGw4",
-    actors: ["Paul Walker", "Vin Diesel"],
+    actors: "Paul Walker, Vin Diesel",
     description:
       " 2 Fast 2 Furious menceritakan kehidupan Brian O’Conner yang telah berhenti dari kepolisian dan kini mencari nafkah dari ajang balap mobil liar. Suatu malam, rekan Brian, seorang mekanik sekaligus promotor balap mobil liar memanggilnya dan menjadikannya salah satu peserta balap. Setelah menaikkan taruhan, Brian yang kini ahli dalam balap mobil berhasil memenangkan posisi pertama. Lalu, ia tampak mendekati seorang wanita cantik bernama Monica.",
   },
